9e9f1ee9fc0c36ebb6f2afa6c5e80f97613d5f43
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2022-10-17 23:21:22 +0900

    feature: 깃허브 회원가입 구현 완료

--- server/src/main/java/com/team_60/Mocco/security/dto/SecurityDto.java
+++ server/src/main/java/com/team_60/Mocco/security/dto/SecurityDto.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 public class SecurityDto {
@@ -15,14 +16,13 @@ public class SecurityDto {
     @AllArgsConstructor
     public static class Login {
         @Email(message = "이메일 형식을 맞추어야 합니다.")
+        @NotBlank(message = "이메일은 빈칸이 아니여야 합니다.")
         private String email;
 
-//        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=/`]).{8,20}$",
-//                message = "비밀번호는 영문과 특수문자 숫자를 포함하며 8자 이상, 20자 이하이어야 합니다.")
-         private String password;
-         public UsernamePasswordAuthenticationToken toAuthentication(){
-             return new UsernamePasswordAuthenticationToken(email,password);
-         }
+        private String password;
+        public UsernamePasswordAuthenticationToken toAuthentication(){
+            return new UsernamePasswordAuthenticationToken(email,password);
+        }
     }
 
 
